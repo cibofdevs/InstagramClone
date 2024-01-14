c878e70c1fb377ce5f2f0520ea27e824b161fa77
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-14 17:51:18 +0700

    add post list adapter to main activity

--- app/src/main/java/com/cibofdevs/instagramclone/MainActivity.kt
+++ app/src/main/java/com/cibofdevs/instagramclone/MainActivity.kt
@@ -3,14 +3,28 @@ package com.cibofdevs.instagramclone
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cibofdevs.instagramclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val vm: MainViewModel by viewModels()
+    private val adapter = PostListAdapter(arrayListOf())
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        vm.getAllPosts()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupUI()
+    }
+
+    fun setupUI() {
+        binding.postsRV.adapter = adapter
+        binding.postsRV.layoutManager = LinearLayoutManager(this)
     }
 }
