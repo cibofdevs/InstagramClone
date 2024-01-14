1e6445f24086290329626a3b720b003959cf96de
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-14 15:27:45 +0700

    perform a network call

--- app/src/main/java/com/cibofdevs/instagramclone/MainActivity.kt
+++ app/src/main/java/com/cibofdevs/instagramclone/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.cibofdevs.instagramclone
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val vm: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        vm.getAllPosts()
         setContentView(R.layout.activity_main)
     }
 }
