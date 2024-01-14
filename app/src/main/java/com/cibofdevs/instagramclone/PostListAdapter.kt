c0e2cff7d7410591114a771a047281d68b7b3b48
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-14 21:22:15 +0700

    update layout

--- app/src/main/java/com/cibofdevs/instagramclone/PostListAdapter.kt
+++ app/src/main/java/com/cibofdevs/instagramclone/PostListAdapter.kt
@@ -2,7 +2,11 @@ package com.cibofdevs.instagramclone
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.cibofdevs.instagramclone.api.InstagramApiService.BASE_URL
 import com.cibofdevs.instagramclone.api.Post
 import com.cibofdevs.instagramclone.databinding.ItemPostBinding
 
@@ -27,6 +31,29 @@ class PostListAdapter(private var posts: List<Post>): RecyclerView.Adapter<PostL
     class PostViewHolder(val binding: ItemPostBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(post: Post) {
             binding.postUsername.text = post.user.username
+            binding.captionUsername.text = post.user.username + ":"
+            binding.captionText.text = post.caption
+
+            val imgContext = binding.postImage.context
+            val circularProgressDrawable = CircularProgressDrawable(imgContext)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+
+            val imageUrl = if (post.imageUrlType == "absolute") post.imageUrl else BASE_URL + post.imageUrl
+
+            Glide.with(imgContext)
+                .load(imageUrl)
+                .centerCrop()
+                .placeholder(circularProgressDrawable)
+                .into(binding.postImage)
+
+            binding.commentLayout.removeAllViews()
+            for (comment in post.comments) {
+                val commentView = TextView(binding.commentLayout.context)
+                commentView.text = "${comment.username}: ${comment.text}"
+                binding.commentLayout.addView(commentView)
+            }
         }
     }
 }
