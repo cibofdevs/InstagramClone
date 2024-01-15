2681eeaf8cdc5c6e6dc4d2583c62ecb2e3b2f674
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-15 08:50:56 +0700

    add onSignup dialog

---
+++ app/src/main/java/com/cibofdevs/instagramclone/SignupDialog.kt
@@ -0,0 +1,44 @@
+package com.cibofdevs.instagramclone
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import com.cibofdevs.instagramclone.databinding.DialogSignupBinding
+
+class SignupDialog(val callback: AuthCallback): DialogFragment() {
+
+    lateinit var binding: DialogSignupBinding
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            binding = DialogSignupBinding.inflate(it.layoutInflater)
+
+            binding.signupButon.setOnClickListener {
+                binding.errorField.visibility = View.GONE
+                val username = binding.usernameET.text.toString()
+                val email = binding.emailET.text.toString()
+                val password = binding.passwordET.text.toString()
+
+                var msg = ""
+                if (username.isEmpty()) msg += "Username cannot be empty\n"
+                if (email.isEmpty()) msg += "Email cannot be empty\n"
+                if (password.isEmpty()) msg += "Password cannot be empty\n"
+
+                if (msg.isEmpty()) {
+                    callback.onSignup(username, email, password)
+                    this@SignupDialog.dismiss()
+                } else {
+                    msg = msg.substring(0, msg.length - 1)
+                    binding.errorField.text = msg
+                    binding.errorField.visibility = View.VISIBLE
+                }
+            }
+
+            return AlertDialog.Builder(it)
+                .setView(binding.root)
+                .create()
+        } ?: return super.onCreateDialog(savedInstanceState)
+    }
+}
