7b040c5f3e2045b25b6aca15bba0eb10ca0b31d2
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-15 08:50:37 +0700

    add onSignup callback

--- app/src/main/java/com/cibofdevs/instagramclone/AuthCallback.kt
+++ app/src/main/java/com/cibofdevs/instagramclone/AuthCallback.kt
@@ -2,4 +2,5 @@ package com.cibofdevs.instagramclone
 
 interface AuthCallback {
     fun onLogin(username: String, password: String)
+    fun onSignup(username: String, email: String, password: String)
 }
