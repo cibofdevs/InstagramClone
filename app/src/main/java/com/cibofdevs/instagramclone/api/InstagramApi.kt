07a9f40a1f32e229d4c26da9eb857dcade84d32f
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-15 08:04:09 +0700

    add login router

--- app/src/main/java/com/cibofdevs/instagramclone/api/InstagramApi.kt
+++ app/src/main/java/com/cibofdevs/instagramclone/api/InstagramApi.kt
@@ -1,9 +1,18 @@
 package com.cibofdevs.instagramclone.api
 
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface InstagramApi {
     @GET("post/all")
     fun getAllPosts(): Call<List<Post>>
+
+    @FormUrlEncoded
+    @POST("login")
+    fun login(@Field("username") username: String,
+              @Field("password") password: String
+    ): Call<UserLoginResponse>
 }
