cb4d554481e176c165e77d052ff63dda1287db37
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-16 14:17:13 +0700

    add http call to upload image

--- app/src/main/java/com/cibofdevs/instagramclone/api/InstagramApi.kt
+++ app/src/main/java/com/cibofdevs/instagramclone/api/InstagramApi.kt
@@ -1,11 +1,15 @@
 package com.cibofdevs.instagramclone.api
 
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface InstagramApi {
     @GET("post/all")
@@ -19,4 +23,10 @@ interface InstagramApi {
 
     @POST("user")
     fun signup(@Body user: UserSignupRequest): Call<UserSignupResponse>
+
+    @Multipart
+    @POST("post/image")
+    fun uploadImage(@Part image: MultipartBody.Part,
+                    @Header("Authorization") auth: String
+    ): Call<ImageUploadResponse>
 }
